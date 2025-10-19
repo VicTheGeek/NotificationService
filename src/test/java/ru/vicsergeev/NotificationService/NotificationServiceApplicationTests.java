8782c9188ca72203f5a563f16da14f48b087a737
That Guy Vic <133888588+VicTheGeek@users.noreply.github.com>
2025-10-19 18:45:28 +0300

    upd

---
+++ src/test/java/ru/vicsergeev/NotificationService/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vicsergeev.NotificationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
